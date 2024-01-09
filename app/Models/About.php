170031c27dc536f94e3c8eefcb5c5835cdc3aaa7
_breakage <dev.iosync@gmail.com>
2024-01-09 16:39:57 +0300

    Adds About model & functions

---
+++ app/Models/About.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class About extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'identifier',
+        'name',
+        'description',
+        'mission',
+        'vision',
+        'core_values',
+        'logo',
+        'added_by',
+        'updated_by'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
