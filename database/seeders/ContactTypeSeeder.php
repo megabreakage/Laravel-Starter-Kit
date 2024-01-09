7a90400a6ae974631eb3e1915052b9b96a5f8bdf
breakage <dev.iosync@gmail.com>
2024-01-10 02:22:40 +0300

    Adds Contact Types

---
+++ database/seeders/ContactTypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ContactType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ContactTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $contact_types = [
+            'email', 'phone_number', 'social', 'fax', 'postal'
+        ];
+
+        foreach ($contact_types as $contact_type) {
+            ContactType::create([
+                'identifier' => generate_identifier(),
+                'name' => $contact_type,
+                'added_by' => 1,
+            ]);
+        }
+    }
+}
