d2da7edeb679edd52182934c34e582d46211cd76
_breakage <dev.iosync@gmail.com>
2024-01-09 12:57:20 +0300

    Adds customer as role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,7 +14,7 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         $roles = [
-            'admin', 'user'
+            'admin', 'user', 'customer'
         ];
 
         foreach($roles as $role){
